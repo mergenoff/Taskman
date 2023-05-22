bd5fc3321a025b69d484b28f681d99739c3c343b
mergenoff <mergenov.timur@mail.ru>
2023-05-22 21:15:36 +0600

    Taskman
    Taskman

---
+++ README.md
@@ -0,0 +1,14 @@
+# iceberg-learn
+This repository was created for the purpose of teaching new students how to work with git, teamwork and working with someone else's code. This project is common and is being done by common efforts through generations
+
+# code appearance guide
+https://www.notion.so/pieceowater/Code-Appearance-Guide-f94e739542f9469fa6b95f27de984225
+
+# design reference
+https://www.figma.com/file/MrGjv6vuyCI0iU9XAHJGMc/taskman?t=xGh1x7drO128WcnQ-1
+
+# goal
+Make a web application for managing your working day.
+- todo-manager with task setting, description, time and deadlines
+- setting up a personal account
+- a system of user accesses to view other people's tasks (superuser, manager, employee)
---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar_images">
+            <img src="./assets/image/fluent_arrow-exit-20-filled.png" alt="">
+            <img src="./assets/image/home-active.png" class="picture" alt="">
+            <img src="./assets/image/ph_users.png"  alt="">
+        </div>
+
+        <div class="sidebar_line"></div>
+
+
+        <div class="sidebar_images">
+            <img src="./assets/image/fluent_dark-theme-20-filled.png" alt="">
+            <img src="./assets/image/Vector@2x.png" alt="">
+
+        </div>
+    </div>
+
+
+    <div class="content">
+        <div class="content_header">
+            <div class="content_header-logo">
+                <img src="./assets/image/fullTaskmanLogo%201%20(1).png" alt="">
+            </div>
+
+            <div class="content_header-search">
+                <img src="./assets/image/search.png" alt="">
+                <span>Search...</span>
+            </div>
+
+            <div class="content_header-profile">
+                <img src="./assets/image/alarm.png" class="alarm" alt="">
+                <img src="./assets/image/user.png" alt="">
+            </div>
+        </div>
+
+        <div class="content_main">
+            <div class="content_info">
+                <img src="./assets/image/executors-group.png" class="pic1" alt="">
+                <img src="./assets/image/gridicons_user-add.png" class="pic2" alt="">
+                <img src="./assets/image/done-condition.png" class="pic3" alt="">
+                <img src="./assets/image/arrow.png" class="pic4" alt="">
+                <div class="content_info-header">
+                    <h1>Go shopping</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim facilis fugit labore laudantium magnam magni necessitatibus nulla quia repellat, ullam! Consectetur est fuga maiores maxime minus nesciunt saepe ullam voluptates?</p>
+                </div>
+
+                <div class="content_info-list">
+                    <h3>Shopping list:</h3>
+
+                    <div class="first">
+                        <input type="radio" id="first">
+                        <label for="first">Lorem ipsums</label>
+                    </div>
+                    <div class="second">
+                        <input type="radio" id="second">
+                        <label for="second">Lorem Ipsum</label>
+                    </div>
+
+                    <div class="third">
+                        <input type="radio" id="third">
+                        <label for="third">Lorem</label>
+                    </div>
+
+                    <div class="forth">
+                        <input type="radio" id="forth">
+                        <label for="forth">Lorem ipsums dolor sit amet</label>
+                    </div>
+
+                    <hr class="orange">
+                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti harum provident quae quisquam quos. Animi corporis cupiditate eaque explicabo fugit id itaque libero pariatur sequi voluptatibus? Amet dolorum eos vel.</p>
+
+                </div>
+            </div>
+
+
+            <div class="content_aside">
+                <div class="content_aside-main">
+                    <div class="content_aside-header">
+                        <div>
+                            <img src="./assets/image/Vectorright.png" style="width: 20px" alt="">
+                        </div>
+                        <div>
+                            <img src="./assets/image/Line%204.png" class="line4" alt="">
+                        </div>
+
+                        <div>Task List</div>
+                    </div>
+
+                    <div class="button">
+                        <input type="radio" id="one">
+                        <label for="one">Go Shopping</label>
+                    </div>
+                    <div class="button">
+                        <input type="radio" id="two">
+                        <label for="two">Make Design for project</label>
+                    </div>
+
+                    <div class="button">
+                        <input type="radio" id="three">
+                        <label for="three">Clean my room</label>
+                    </div>
+
+                    <div class="button">
+                        <input type="radio" id="four">
+                        <label for="four">Buy some milk</label>
+                    </div>
+
+                    <div class="button">
+                        <input type="radio" id="five">
+                        <label for="five">Create task manager</label>
+                    </div>
+                </div>
+
+                
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
